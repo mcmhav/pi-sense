76a93655d70440f774d4d222bebab48623f6d494
mcmhav <mcmhav@gmail.com>
2017-11-27 01:47:31 +0100

    add message file

---
+++ message.py
@@ -0,0 +1,18 @@
+from sense_hat import SenseHat
+from sys import argv
+import argparse
+
+parser = argparse.ArgumentParser(description='Sending a message.')
+parser.add_argument(
+    'message',
+    # metavar='N',
+    type=str,
+    # nargs='+',
+    default='Hello world'
+    help='The message to be shown'
+)
+
+args = parser.parse_args()
+
+sense = SenseHat()
+sense.show_message(args.message)
--- randomColor.py
+++ randomColor.py
@@ -10,12 +10,14 @@ green = (0, 255, 0)
 white = (255, 255, 255)
 yellow = (255, 255, 0)
 
+
 # Generate a random colour
 def pick_random_colour():
-  random_red = randint(0, 255)
-  random_green = randint(0, 255)
-  random_blue = randint(0, 255)
-  return (random_red, random_green, random_blue)
+    random_red = randint(0, 255)
+    random_green = randint(0, 255)
+    random_blue = randint(0, 255)
+    return (random_red, random_green, random_blue)
+
 
 sense.show_letter("L", pick_random_colour())
 sleep(1)
