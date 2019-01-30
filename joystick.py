b507942303b3714b2107d4d4c6d085cd45b9b12f
mcmhav <mcmhav@gmail.com>
2019-01-30 17:21:58 +0100

    asdfadsf

--- joystick.py
+++ joystick.py
@@ -7,22 +7,22 @@ w = (255, 255, 255)
 
 sense.clear()
 while True:
-  for event in sense.stick.get_events():
-    # Check if the joystick was pressed
-    if event.action == "pressed":
+    for event in sense.stick.get_events():
+        # Check if the joystick was pressed
+        if event.action == "pressed":
 
-      # Check which direction
-      if event.direction == "up":
-        sense.show_letter("U")      # Up arrow
-      elif event.direction == "down":
-        sense.show_letter("D")      # Down arrow
-      elif event.direction == "left":
-        sense.show_letter("L")      # Left arrow
-      elif event.direction == "right":
-        sense.show_letter("R")      # Right arrow
-      elif event.direction == "middle":
-        sense.show_letter("M")      # Enter key
+            # Check which direction
+            if event.direction == "up":
+                sense.show_letter("U")      # Up arrow
+            elif event.direction == "down":
+                sense.show_letter("D")      # Down arrow
+            elif event.direction == "left":
+                sense.show_letter("L")      # Left arrow
+            elif event.direction == "right":
+                sense.show_letter("R")      # Right arrow
+            elif event.direction == "middle":
+                sense.show_letter("M")      # Enter key
 
-      # Wait a while and then clear the screen
-      sleep(0.5)
-      sense.clear()
+            # Wait a while and then clear the screen
+            sleep(0.5)
+            sense.clear()
--- pixelate.py
+++ pixelate.py
@@ -4,100 +4,112 @@ from sense_hat import SenseHat
 from time import sleep
 import picamera
 
-camera = picamera.PiCamera()
-camera.capture('image.jpg')
+IMAGE_LOCATION = 'image.jpg'
 
-pixels = 8
-pixelSize = 100
 
-image = Image.open('image.jpg')
+def take_picture():
+    camera = picamera.PiCamera()
+    camera.capture(IMAGE_LOCATION)
 
-image_width = image.size[0]
-image_height = image.size[1]
 
-size_diff = image_width - image_height
+def convert_image_to_pixels():
+    pixels = 8
+    pixel_size = 100
 
-width_reduction = 0
-height_reduction = 0
+    image = Image.open(IMAGE_LOCATION)
 
-new_width_min = 0
-new_width_max = image_width
-new_height_min = 0
-new_height_max = image_height
-if size_diff > 0:
-    new_width_min = int(math.fabs(size_diff) / 2)
-    new_width_max = int(image_width - math.fabs(size_diff) / 2)
-elif size_diff < 0:
-    new_height_min = int(math.fabs(size_diff) / 2)
-    new_height_max = int(image_height - math.fabs(size_diff) / 2)
+    image_width = image.size[0]
+    image_height = image.size[1]
 
-image = image.crop(
-    (
-        new_width_min,
-        new_height_min,
-        new_width_max,
-        new_height_max
+    size_diff = image_width - image_height
+
+    width_reduction = 0
+    height_reduction = 0
+
+    new_width_min = 0
+    new_width_max = image_width
+    new_height_min = 0
+    new_height_max = image_height
+    if size_diff > 0:
+        new_width_min = int(math.fabs(size_diff) / 2)
+        new_width_max = int(image_width - math.fabs(size_diff) / 2)
+    elif size_diff < 0:
+        new_height_min = int(math.fabs(size_diff) / 2)
+        new_height_max = int(image_height - math.fabs(size_diff) / 2)
+
+    image = image.crop(
+        (
+            new_width_min,
+            new_height_min,
+            new_width_max,
+            new_height_max
+        )
     )
-)
 
-pixelSize = math.floor(image.size[0]/pixels)
+    pixel_size = math.floor(image.size[0]/pixels)
 
-# print(int(math.floor(image.size[0]/pixelSize)))
-# print(int(math.floor(image.size[1]/pixelSize)))
-# print(Image.NEAREST)
+    # print(int(math.floor(image.size[0]/pixel_size)))
+    # print(int(math.floor(image.size[1]/pixel_size)))
+    # print(Image.NEAREST)
 
-# image = image.resize(
-#     (
-#         int(math.floor(image.size[0]/pixelSize)),
-#         int(math.floor(image.size[1]/pixelSize))
-#     ),
-#     Image.NEAREST
-# )
-# image = image.resize(
-#     (
-#       int(math.floor(image.size[0]*pixelSize)),
-#       int(math.floor(image.size[1]*pixelSize))
-#     ),
-#     Image.NEAREST
-# )
+    # image = image.resize(
+    #     (
+    #         int(math.floor(image.size[0]/pixel_size)),
+    #         int(math.floor(image.size[1]/pixel_size))
+    #     ),
+    #     Image.NEAREST
+    # )
+    # image = image.resize(
+    #     (
+    #       int(math.floor(image.size[0]*pixel_size)),
+    #       int(math.floor(image.size[1]*pixel_size))
+    #     ),
+    #     Image.NEAREST
+    # )
 
-print('==============')
-print(pixels)
-print(Image.NEAREST)
-print('==============')
+    print('==============')
+    print(pixels)
+    print(Image.NEAREST)
+    print('==============')
 
-maxsize = (pixels, pixels)
-image.thumbnail(maxsize, Image.NEAREST)
-image.save('output.jpg')
+    maxsize = (pixels, pixels)
+    image.thumbnail(maxsize, Image.NEAREST)
+    image.save('output.jpg')
 
-sense = SenseHat()
-
-
-def test():
-    pixel = image.load()
-    for i in range(0, image.size[0]):
-        for j in range(0, image.size[1]):
-            sense.set_pixel(i, j, pixel[i, j])
-
-            print(pixel[i, j])
-            print(pixel[i, j])
+    return image, pixel_size
 
 
-test()
+def pixelate():
+    image, pixel_size = convert_image_to_pixels()
 
-sleep(10)
+    sense = SenseHat()
 
-sense.clear()
+    def test():
+        pixel = image.load()
+        for i in range(0, image.size[0]):
+            for j in range(0, image.size[1]):
+                sense.set_pixel(i, j, pixel[i, j])
+
+                print(pixel[i, j])
+                print(pixel[i, j])
+
+    test()
+
+    sleep(10)
+
+    sense.clear()
+
+    def border():
+        background_color = (0,)*3
+        pixel = image.load()
+
+        for i in range(0, image.size[0], pixel_size):
+            for j in range(0, image.size[1], pixel_size):
+                for r in range(pixel_size):
+                    pixel[i+r, j] = background_color
+                    pixel[i, j+r] = background_color
+
+    # image.save('output.jpg')
 
 
-def border():
-    backgroundColor = (0,)*3
-    pixel = image.load()
-
-    for i in range(0, image.size[0], pixelSize):
-        for j in range(0, image.size[1], pixelSize):
-            for r in range(pixelSize):
-                pixel[i+r, j] = backgroundColor
-                pixel[i, j+r] = backgroundColor
-
-# image.save('output.jpg')
+pixelate()
